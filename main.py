7c908a2c2b34e5674eb7ac84b0a5ad471e147adc
Robin Tainton <robintainton0509@gmail.com>
2021-12-20 01:23:07 +0200

    improving user experience and save states that user didnt know into .txt file for further learning

--- main.py
+++ main.py
@@ -11,18 +11,28 @@ x = turtle.Turtle()
 x.penup()
 x.hideturtle()
 states_data = pd.read_csv("50_states.csv")
-
+states_list = states_data.state.to_list()
 while len(entered_states) < 50:
 
-    state_name = screen.textinput(title=f"{len(entered_states)}/50 States", prompt="Enter the name of a state").title()
+    state_name = screen.textinput(title=f"{len(entered_states)}/50 States",
+                                  prompt="Enter the name of a state").strip().title()
     if state_name in entered_states:
         print(f"{state_name} was already entered")
-    elif state_name in states_data.state.to_list():
+    elif state_name in states_list:
         entered_states.append(state_name)
         entered_state_data = states_data[states_data.state == state_name]
         x.goto(int(entered_state_data.x), int(entered_state_data.y))
         x.write(state_name, align="center")
+    elif state_name == "Exit":
+        break
     else:
         print(f"{state_name} is not a state")
+
+with open("states_to_learn.txt", mode="a") as file:
+    file.truncate(0)
+    for state in states_list:
+        if state not in entered_states:
+            file.write(f"{state}\n")
+
 
 screen.exitonclick()
